256ddf39aa734372f1ef8152569e9fa4f9f3ce7a
kingaGniedziejko <47779092+kingaGniedziejko@users.noreply.github.com>
2020-10-19 22:44:01 +0200

    custom google map setup

---
+++ app/src/main/java/com/example/maraudersmapoflondon/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.maraudersmapoflondon;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        try {
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.retro_map_style));
+
+        } catch (Resources.NotFoundException e) {
+
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(51.5074, -0.1278)));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+    }
+}
